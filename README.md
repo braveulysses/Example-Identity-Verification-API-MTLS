d91c843e97061f77fb3d27aba6d718b660ce0f7f
Jacob Childress <jacobc@gmail.com>
2020-03-19 13:38:29 -0500

    Handle MTLS manually rather than using passport
    Couldn't get passport-client-cert to work, so the server
    now just handles cert authentication itself.

--- README.md
+++ README.md
@@ -1,12 +1,80 @@
 # Example Identity Verification API with MTLS
 
-This is a very simple REST API for use with a tutorial at my day job. 
-This is a fork of [a similar example API](https://github.com/braveulysses/Example-Identity-Verification-API), 
-but with MTLS support. It probably doesn't work yet.
+This is a very simple REST API that's for use with an internal tutorial at my company. 
 
-## How to run it
+This is a fork of [a similar example API](https://github.com/braveulysses/Example-Identity-Verification-API) 
+that adds crude MTLS (client certificate authentication) support to the server. 
+Unlike the other example API, which is intended to be easily hosted on Glitch or 
+Heroku, it's easier if you just run this one yourself.
+
+Requests to `/users/:userid` are "protected", meaning that they will be rejected 
+if the client does not provide a client certificate with a specific CN value, 
+which is hard-coded to be "alice".
+
+## How to run the server
+
+Install [NodeJS](https://nodejs.org/en/).
+
+Install [Yarn](https://yarnpkg.com/).
+
+Install the project dependencies.
 
 ```bash
 yarn install
+```
+
+Start the server. By default, it will listen on port 3000 using HTTPS and 
+a self-signed certificate.
+
+```bash
 yarn start
+```
+
+## Testing
+
+A couple example client certificates and keys are provided in the `certs` 
+directory. By default, the `01-alice.pem` certificate will be accepted by the 
+server, while the `02-carol.pem` certificate will be rejected.
+
+These examples use [httpie](https://httpie.org), but you can use any HTTP 
+client that pleases you.
+
+Make a request with the 'alice' cert.
+
+```bash
+$ http --verify=no --cert=certs/01-alice.pem --cert-key=certs/01-alice.key https://localhost:3000/users/user.1
+HTTP/1.1 200 OK
+Connection: keep-alive
+Content-Length: 112
+Content-Type: application/json; charset=utf-8
+Date: Thu, 19 Mar 2020 18:32:19 GMT
+ETag: W/"70-G9oTvFNJxXG0Gn+LN3OPzv3T+Bc"
+Vary: Accept
+X-Powered-By: Express
+
+{
+    "documentType": "driver's license",
+    "issuingCountry": "ES",
+    "nationality": "ES",
+    "username": "user.1",
+    "verified": true
+}
+```
+
+Make a request with the 'carol' cert.
+
+```bash
+$ http --verify=no --cert=certs/02-carol.pem --cert-key=certs/02-carol.key https://localhost:3000/users/user.1
+HTTP/1.1 403 Forbidden
+Connection: keep-alive
+Content-Length: 35
+Content-Type: application/json; charset=utf-8
+Date: Thu, 19 Mar 2020 18:01:55 GMT
+ETag: W/"23-eZc6O/uj1EEa6SCtdknBZrooqx4"
+Vary: Accept
+X-Powered-By: Express
+
+{
+    "error": "Client cert auth failed"
+}
 ```
--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
 import express from 'express';
+import fs from 'fs';
 import https from 'https';
 import morgan from 'morgan';
-import passport from 'passport';
 import path from 'path';
 import User from './models/User';
 
-import { Strategy as ClientCertStrategy } from 'passport-client-cert';
-
 // A list of valid user IDs
-const users = [ 'alice' ];
+const USERS = [ 'alice' ];
 
-const authOptions = {
-  requestCert: true,
-  rejectUnauthorized: true
-};
+// The directory containing the server cert
+const CERT_DIR = 'certs';
 
-const _authenticate = (cert, done) => {
-  const subject = cert.subject;
-  const msg = 'Attempting MTLS authentication';
+// Server and CA cert files
+const SERVER_KEY = '03-server.key';
+const SERVER_CERT = '03-server.pem';
+const CA_CERT = 'ca.pem';
 
-  if (!subject) {
-    console.log(msg + ' ✘ - no subject');
+// Confirm that a certificate file exists and exit if not.
+const _checkCertFile = (certFile) => {
+  const f = path.join(CERT_DIR, certFile);
+  if (!fs.existsSync(f)) {
+    console.error(f + ' not found; cannot start HTTPS server');
+    process.exit(1);
   }
 };
 
+// Looks up a client cert's CN value and matches it against the array of
+// allowed users.
+const _lookupUser = (cn) => {
+  return USERS.includes(cn);
+};
+
+// Client cert authentication.
+const _authenticate = (cert) => {
+  const subject = cert.subject;
+  let msg = 'Attempting MTLS authentication';
+
+  console.log(msg);
+
+  if (!subject) {
+    console.log(msg + ' ✘ - no subject');
+    return false;
+  } else if (!subject.CN) {
+    console.log(msg +  '✘ - no client CN');
+    return false;
+  } else {
+    const cn = subject.CN;
+
+    const user = _lookupUser(cn);
+    const msg = 'Authenticating ' +  cn + ' with certificate';
+    if (!user) {
+      console.log(msg + ' ✘ - no such user');
+      return false;
+    } else {
+      console.log(msg + ' - ✔');
+      return true;
+    }
+  }
+};
+
+// Responds to a request with the about page.
 const _sendDefaultResponse = (response) => {
   response.format({
     html: () => {
@@ -32,38 +68,59 @@ const _sendDefaultResponse = (response) => {
   });
 };
 
+// Main
+console.log('Starting up');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(morgan('dev'));
-app.use(passport.initialize());
-passport.use(new ClientCertStrategy(_authenticate));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+// Specific user
 app.get('/users/:username', (request, response) => {
-  const user = new User(request.params.username);
-  response.format({
-    json: () => {
-      response.status(200).json(user);
-    }
-  });
+  const cert = request.connection.getPeerCertificate();
+  const authenticated = _authenticate(cert, response);
+  if (authenticated) {
+    console.log('Authenticated');
+    const user = new User(request.params.username);
+    response.format({
+      json: () => {
+        response.status(200).json(user);
+      }
+    });
+  } else {
+    console.log('Not authenticated');
+    response.format({
+      json: () => {
+        response.status(403).json({ error: "Client cert auth failed" });
+      }
+    });
+  }
 });
 
+// Root endpoint
 app.get('/', (request, response) => {
   _sendDefaultResponse(response);
 });
 
-app.get('/users',
-    passport.authenticate('client-cert', { session: false }),
-    (request, response) => {
+// Base users endpoint
+app.get('/users', (request, response) => {
   _sendDefaultResponse(response);
 });
 
+console.log('Setting up TLS');
+[ SERVER_CERT, SERVER_KEY, CA_CERT ].forEach(_checkCertFile);
+const tlsOptions = {
+  key: fs.readFileSync(path.join(CERT_DIR, SERVER_KEY)),
+  cert: fs.readFileSync(path.join(CERT_DIR, SERVER_CERT)),
+  requestCert: true,
+  rejectUnauthorized: false,
+  ca: fs.readFileSync(path.join(CERT_DIR, CA_CERT))
+};
 const port = process.env.PORT || 3000;
-// app.listen(port, () => console.log('Listening to port ' + port));
 
-https.createServer(authOptions, app).listen(process.env.PORT || 3000, () => {
-  console.log("Listening to port 3000")
+https.createServer(tlsOptions, app).listen(process.env.PORT || 3000, () => {
+  console.log('Listening to port ' + port);
 });
 
